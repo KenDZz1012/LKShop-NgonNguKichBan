52a0dac068c87de3798bf1cb39b3a7528efa30ab
KENDZ\kient <kienteo1012@gmail.com>
2022-11-21 10:06:19 +0700

    code new

---
+++ LKShop-BE/src/apps/services/Bundle/Repositories/BundleRepository.ts
@@ -0,0 +1,32 @@
+import Bundle from "../DTO/Bundle";
+import BundleCreate from "../DTO/BundlleCreate";
+import BundleFilter from "../DTO/BundleFilter";
+import BundleUpdate from "../DTO/BundleUpdate";
+
+const getListBundleHandler = async () => {
+    
+}
+
+const getBundleByIdHandler = async () => {
+
+}
+
+const createBundleHandler = async () => {
+
+}
+
+const updateBundleHandler = async () => {
+
+}
+
+const deleteBundleHandler = async () => {
+
+}
+
+export {
+    getListBundleHandler,
+    getBundleByIdHandler,
+    createBundleHandler,
+    updateBundleHandler,
+    deleteBundleHandler
+}
